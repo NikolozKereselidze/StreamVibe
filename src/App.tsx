6cc80513d4f9fc75981c9d2d2a00445a86c11a8d
Nika <Nika@users-MacBook-Pro.local>
2025-01-09 20:23:32 +0400

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import MovieSearch from "./test";
+
+function App() {
+  return (
+    <>
+      <div>
+        <MovieSearch />
+      </div>
+    </>
+  );
+}
+
+export default App;

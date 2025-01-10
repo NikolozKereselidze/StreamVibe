959cce2b0f05fe3ded2cbb6192e7ea7c2b38da6b
Nika <Nika@users-MacBook-Pro.local>
2025-01-10 22:31:58 +0400

    feat: expand SearchResult interface with new values

--- src/types/search.d.ts
+++ src/types/search.d.ts
@@ -13,4 +13,13 @@ export interface SearchResult {
   vote_average?: number; // Movies and TV shows
   known_for_department?: strinng; // Person
   profile_path?: string; // Person
+  runtime?: number; // Movie runtime in minutes
+  genres?: string[]; // Genres as an array of names
+  releaseYear?: string; // Extracted year from release_date
+  ageRating?: string; // Simplified age rating
+  seasons?: number; // Tv show seasons
+  gender?: number; // Person gender
+  known_for?: {
+    original_title: string;
+  }[];
 }

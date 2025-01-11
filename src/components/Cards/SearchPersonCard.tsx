c24619968f81d5ed3495dd7c6d25238d8bb12ea1
Nika <Nika@users-MacBook-Pro.local>
2025-01-12 00:39:10 +0400

    refactor: Replace individual card components with Search-specific versions
    - Replace MovieCard, TvCard, and PersonCard with SearchMovieCard, SearchTvCard, and SearchPersonCard
    - Remove unused card components (MovieCard, TvCard, PersonCard)

---
+++ src/components/Cards/SearchPersonCard.tsx
@@ -0,0 +1,46 @@
+import { SearchResult } from "../../types/search";
+import styles from "../../styles/Results.module.css";
+
+interface PersonCardProps {
+  item: SearchResult;
+}
+
+const SearchPersonCard: React.FC<PersonCardProps> = ({ item }) => (
+  <>
+    {item.profile_path && (
+      <img
+        src={`https://image.tmdb.org/t/p/w500${item.profile_path}`}
+        alt={`${item.name}`}
+      />
+    )}
+
+    <div className={`${styles.details} ${styles.detailsPerson}`}>
+      <div className={styles.detailsHeaderPerson}>
+        {item.name && (
+          <>
+            <h3>Full Name</h3>
+            <h4 className={styles.personName}>{item.name}</h4>
+          </>
+        )}
+        {item.gender && (
+          <>
+            <h3>Gender</h3>
+            <h4 className={styles.gender}>
+              {item.gender === 2 ? "Male" : "Female"}
+            </h4>
+          </>
+        )}
+      </div>
+      <div className={styles.detailsFooterPerson}>
+        <h3>Known For</h3>
+        {item.known_for?.map((i, index) => (
+          <h2 className={styles.knownFor} key={`${index}-${i}`}>
+            {i.original_title}
+          </h2>
+        ))}
+      </div>
+    </div>
+  </>
+);
+
+export default SearchPersonCard;
---
+++ src/components/Cards/SearchTvCard.tsx
@@ -0,0 +1,33 @@
+import { SearchResult } from "../../types/search";
+import styles from "../../styles/Results.module.css";
+
+interface TVCardProps {
+  item: SearchResult;
+}
+
+const SearchTvCard: React.FC<TVCardProps> = ({ item }) => (
+  <>
+    {item.poster_path && (
+      <img
+        src={`https://image.tmdb.org/t/p/w500${item.poster_path}`}
+        alt={`${item.name} poster`}
+      />
+    )}
+
+    <div className={styles.details}>
+      <div className={styles.detailsHeader}>
+        <h4 className={styles.age}>{item.ageRating}</h4>
+        {item.seasons && (
+          <h4 className={styles.season}>{item.seasons} seasons</h4>
+        )}
+      </div>
+      <div className={styles.detailsFooter}>
+        {item.genres?.map((i) => (
+          <h2 key={`${item.id}-${i}`}>{i}</h2>
+        ))}
+      </div>
+    </div>
+  </>
+);
+
+export default SearchTvCard;

db78369373102801811a4256dd83e785ad2a6c0e
Nika <Nika@users-MacBook-Pro.local>
2025-01-12 00:39:00 +0400

    feat: Add routing with BrowserRouter and conditional Layout
    - Integrate BrowserRouter and Routes for app navigation
    - Create Layout component to conditionally render <Search> except on / and /home

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,31 @@
+import { ReactNode } from "react";
+import { BrowserRouter, Route, Routes, useLocation } from "react-router-dom";
 import "./App.css";
 import Search from "./components/Search/Search";
+import Home from "./pages/Home";
 
 function App() {
   return (
+    <BrowserRouter>
+      <Layout>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="home" element={<Home />} />
+        </Routes>
+      </Layout>
+    </BrowserRouter>
+  );
+}
+
+function Layout({ children }: { children: ReactNode }) {
+  const location = useLocation();
+
+  const showSearch = !["/", "/home"].includes(location.pathname);
+
+  return (
     <>
-      <div>
-        <Search />
-      </div>
+      {showSearch && <Search />}
+      {children}
     </>
   );
 }

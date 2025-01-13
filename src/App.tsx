7e7ea545e077add029157af4e7c62bb8ce43c64e
Nika <Nika@users-MacBook-Pro.local>
2025-01-13 15:51:49 +0400

    feat: add Search route

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
-import { ReactNode } from "react";
-import { BrowserRouter, Route, Routes, useLocation } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
 import Search from "./components/Search/Search";
 import Home from "./pages/Home";
@@ -7,26 +6,12 @@ import Home from "./pages/Home";
 function App() {
   return (
     <BrowserRouter>
-      <Layout>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="home" element={<Home />} />
-        </Routes>
-      </Layout>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="home" element={<Home />} />
+        <Route path="/search" element={<Search />} />
+      </Routes>
     </BrowserRouter>
-  );
-}
-
-function Layout({ children }: { children: ReactNode }) {
-  const location = useLocation();
-
-  const showSearch = !["/", "/home"].includes(location.pathname);
-
-  return (
-    <>
-      {showSearch && <Search />}
-      {children}
-    </>
   );
 }
 

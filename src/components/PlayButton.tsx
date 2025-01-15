33af218cc44a788fe92d4c42101f1e17bad2c28c
Nika <Nika@users-MacBook-Pro.local>
2025-01-15 18:47:31 +0400

    refactor: create reusable PlayButton component

--- src/components/PlayButton.tsx
+++ src/components/PlayButton.tsx
@@ -1,12 +1,19 @@
 import { PlayIcon } from "@heroicons/react/24/outline";
 
-const PlayButton = ({ title }: { title: string }) => (
-  <div className="welcomeButton">
-    <button>
-      <PlayIcon />
-      {title}
-    </button>
-  </div>
-);
+interface PlayButtonProps {
+  title: string;
+  onClick?: () => void;
+}
+
+const PlayButton: React.FC<PlayButtonProps> = ({ title, onClick }) => {
+  return (
+    <div className="welcomeButton">
+      <button onClick={onClick}>
+        <PlayIcon />
+        {title}
+      </button>
+    </div>
+  );
+};
 
 export default PlayButton;

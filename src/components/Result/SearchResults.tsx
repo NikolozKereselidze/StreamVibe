96adde9bfb3bc6aeaf38da103d8e7074f85af993
Nika <Nika@users-MacBook-Pro.local>
2025-01-20 21:37:41 +0400

    feat: add  and  components in  page

--- src/components/Result/SearchResults.tsx
+++ src/components/Result/SearchResults.tsx
@@ -1,6 +1,8 @@
 import { SearchResult } from "../../types/search";
 import SearchResultItem from "./SearchResultItem";
 import styles from "../../styles/Results.module.css";
+import Footer from "../Footer";
+import Ad from "../Ad";
 
 interface SearchResultsProps {
   results: SearchResult[];
@@ -8,13 +10,19 @@ interface SearchResultsProps {
 
 const SearchResults: React.FC<SearchResultsProps> = ({ results }) => {
   return (
-    <div className={styles.wrapper}>
-      {results.length === 0 ? (
-        <p>No results found.</p>
-      ) : (
-        results.map((item) => <SearchResultItem key={item.id} item={item} />)
-      )}
-    </div>
+    <>
+      <div className={styles.wrapper}>
+        {results.length === 0 ? (
+          <p>No results found.</p>
+        ) : (
+          results.map((item) => <SearchResultItem key={item.id} item={item} />)
+        )}
+      </div>
+      <div className="section">
+        <Ad />
+      </div>
+      <Footer />
+    </>
   );
 };
 

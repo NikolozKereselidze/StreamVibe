b0a359012556af9cedc18d73850e54ffda5d4fb1
Nika <Nika@users-MacBook-Pro.local>
2025-01-21 01:25:34 +0400

    feat: lazy load pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,21 @@
+import { lazy } from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
-import Search from "./components/Search/Search";
-import Home from "./pages/Home";
-import Result from "./pages/Result";
-import Support from "./pages/Support";
-import Subscriptions from "./pages/Subscriptions";
-import Media from "./pages/Media";
+const Home = lazy(() => import("./pages/Home"));
+const Search = lazy(() => import("./components/Search/Search"));
+const Result = lazy(() => import("./pages/Result"));
+const Support = lazy(() => import("./pages/Support"));
+const Subscriptions = lazy(() => import("./pages/Subscriptions"));
+const Media = lazy(() => import("./pages/Media"));
+import Scroll from "./utility/Scroll";
 
 function App() {
   return (
     <BrowserRouter>
+      <Scroll />
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="home" element={<Home />} />
+        <Route path="/home" element={<Home />} />
         <Route path="/search" element={<Search />} />
         <Route path="/result" element={<Result />} />
         <Route path="/media" element={<Media />} />
